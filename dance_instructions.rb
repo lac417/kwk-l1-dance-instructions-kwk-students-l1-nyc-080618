3c63857b57fcce3006467e86ce3c7f083f8ca922
lac417 <lac417@gmail.com>
2018-08-07 13:57:36 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -26,4 +26,20 @@ end
 def lasso
   puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left"
   puts "lift right arm up and point right elbow diagonally to the right"
-  
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+  puts "Do this 7 more times!"
+end
+
+def bust_a_move
+  puts "Be cool and by snazzy ;)"
+end
+
+def gangnam_style
+  starting_stance
+  base_footwork
+  skip_step("right foot")
+  bob_the_reins
+  skip_step("left foot")
+  lasso
+  bust_a_move
+end
